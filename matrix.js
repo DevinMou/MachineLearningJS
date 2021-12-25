6c74c409b1c9a60d635edecf235ce48f538a0435
DevinMou <906348933@qq.com>
2021-12-25 17:40:39 +0800

    dobule-shift-qr

--- matrix.js
+++ matrix.js
@@ -192,6 +192,70 @@ function splitM(A, [rs, re], [cs, ce]) {
   }
 }
 
+function concatM(...As) { // [A B] (A,B,false)
+  const last = As[As.length-1]
+  const typeLast = typeof last
+  const mode = typeLast === "boolean" ?  last ? 2 : 1 : 0
+  mode && As.splice(As.length-1,1)
+  const rcs = As.map(a=>getMRC(a))
+  const equalR = !rcs.find(e => e[0]!==rcs[0][0])
+  const equalC = !rcs.find(e => e[1]!==rcs[0][1])
+  function sum (A) {
+    return A.reduce((a,b)=>(a+=b,a))
+  }
+  if(mode ? mode===1 && equalR || mode===2 && equalC : equalR||equalC){
+    const rcsT = transpose(rcs)
+    const d = mode ? mode===1 : equalR
+    return createMatrix(d? rcs[0][0] : sum(rcsT[0]),d? sum(rcsT[1]):rcs[0][1],(r,c)=>{
+      if(d){
+        const i = rcsT[1].findIndex(e=>c<e?true:(c-=e,false))
+        return As[i][r][c]
+      }else{
+        const i = rcsT[0].findIndex(e=>r<e?true:(r-=e,false))
+        return As[i][r][c]
+      }
+    })
+  }else{
+    return null
+  }
+}
+
+function Hh(v) {
+  const len = v.length
+  if (len===1){
+    return [v[0],[[1]]]
+  }
+  const nv = norm(v)
+  if (len === 2) {
+    const s = v[0] / nv
+    const c = v[1] / nv
+    return [[nv,0],[[s,c],[c,-s]]]
+  }
+  const w = createMatrix(len,1,(r,c)=>r===0&&c===0?nv:0)
+  const t = MPO(v,w, MMath.sub)
+  const P = MPO(createMatrix(len,len,(r,c)=>r===c?1:0),MPO(MT(t,transpose(t)),2/norm(t,2,true),MMath.time),MMath.sub)
+  return [w, P]
+}
+
+function HR(A) { // Hessenburg Reduction
+  const [ra, ca] = getMRC(A)
+  let TA = copy(A)
+  let P = null
+  for(let i=0;i<ca-1;i++){
+    const v = TA.slice(1+i-ca).map(e=>e[i])
+    const [w,p] = Hh(v)
+    const tP = createMatrix(ra,ca,(r,c)=>r>i&&c>i?(p[r-1-i][c-1-i]):r===c?1:0)
+    P = P ? MT(P,tP) : tP
+    const tp = transpose(p)
+    const X0 = splitM(TA,[0,i+1],[0,i+1])
+    const X1 = MT(p,splitM(TA,[i+1],[0,i+1]))
+    const X2 = MT(MT(p,splitM(TA,[i+1],[i+1])),tp)
+    const X3 = MT(splitM(TA,[0,i+1],[i+1]),tp)
+    TA = concatM(concatM(X0,X3),concatM(X1,X2),true)
+  }
+  return [P,TA]
+}
+
 function HH(A) {
   // Householder
   const [ra, ca] = getMRC(A);
@@ -203,14 +267,7 @@ function HH(A) {
     const rx = X.length;
     const XT = transpose(X);
     const a = XT[0];
-    const na = norm(a);
-    const b = createMatrix(rx, 1, (r, c) => (r === 0 && c === 0 ? na : 0));
-    const t = MPO(a, b, MMath.sub);
-    const th = MPO(
-      createMatrix(rx, rx, (r, c) => (r === c ? 1 : 0)),
-      MPO(MT(t, transpose(t)),2/norm(t,2,true), MMath.time),
-      MMath.sub
-    );
+    const [b,th] = Hh(a)
     const [rh, ch] = getMRC(th);
     const h = createMatrix(ra, ra, (r, c) => {
       if (r >= ra - rh && c >= ra - rh) {
@@ -237,9 +294,9 @@ function Givens(A) {
     const t1 = TX[i][a];
     const t2 = TX[i][b];
     let Q = null;
-    if (t2 === 0) {
+    if (is0(t2)) {
       return;
-    } else if (t1 === 0) {
+    } else if (is0(t1)) {
       TX.forEach((e) => {
         [e[a], e[b]] = [e[b], e[a]];
       });
@@ -302,28 +359,42 @@ function is0 (n) { // equalZero
   return n < 0 ? n > -1e-12 : n < 1e-12
 }
 
-function ET (A) { // Elementary transformation
+function ET (A, reduce, augmented) { // Elementary transformation
   const [ra, ca] = getMRC(A);
   if(ra!==null){
-    const CA = copy(A)
+    const [ram,cam] = augmented ? getMRC(augmented) : [0,0]
+    const CA = ram ? concatM(A,augmented) : copy(A)
     const min = ra < ca ? ra : ca;
     const tempA = []
     for(let i = 0 ; i<min;i++){
-      const ni = CA.findIndex(e => !is0(e))
+      const ni = CA.findIndex((e, j) => j<ca && !is0(e[i]))
       if(ni!==-1){
         const tr = CA[ni].map(e=>e/CA[ni][i])
         CA.splice(ni,1)
         tempA.push(tr)
         CA.forEach(e=>{
           const coe = e[i]
-          for(let j=i;j<ca;j++){
+          for(let j=i;j<ca+cam;j++){
             e[j]-=coe*tr[j]
             is0(e[j])&&(e[j]=0);
           }
         })
       }
     }
-    return [...tempA,...CA]
+    if(reduce){
+      for(let i=1;i<tempA.length;i++){
+        const item = tempA[i]
+        const ni = item.findIndex(e => !is0(e))
+        for(let j=0;j<i;j++){
+          const n = tempA[j][ni]/item[ni]
+          tempA[j] = tempA[j].map((e,k)=>e-n*item[k])
+        }
+      }
+    }
+    if(CA.find(e=>e.find((t,i)=>i<ca && !is0(t)))){
+      return null
+    }
+    return augmented ? [[splitM(tempA,[0,ra],[0,ca]),splitM(CA,[0,ra],[0,ca])],[splitM(tempA,[0,ra],[ca]),splitM(CA,[0,ra],[ca])]] : [tempA,CA]
   }
   return null
 }
@@ -357,6 +428,28 @@ function SOE (E, h) { // solution of equation, h: Homogeneous linear
   return RA.map(e => e.reverse())
 }
 
+function AXB (E, hasB) { // resolve Ax=b
+  E = copy(E)
+  const len = E[0].length + (hasB ? 1 : 0)
+  let r = len - 1
+  const RI = Object.keys(Array(len-1).fill()).map(e=>+e)
+  E.forEach(e=>{
+    const i = e.findIndex(t => !is0(t))
+    if(i>-1){
+      RI.splice(RI.indexOf(i),1)
+      r-=1
+    }
+    hasB&&e.push(0)
+  })
+  RI.forEach(e => {
+    const t = Array(len).fill(0)
+    t[e] = 1
+    E.splice(e,0,t)
+  })
+  const RA = transpose(E.slice(0,len-1)).filter((e, i) => RI.includes(i)||i===len-1)
+  return [RA[RA.length-1],...RA.slice(0,-1).map(e=>e.map(t=>-t))]
+}
+
 function vecI(v){ // vector identity
   const n = norm(v)
   return n ? v.map(e => e/n) : v
@@ -365,4 +458,94 @@ function vecI(v){ // vector identity
 function getFeatVecByval (A, lam) {
   const HA = copy(A,(e,i,j)=>i===j?(e-lam):e)
   return SOE(ET(HA),true).slice(1).map(e=>vecI(e))
+}
+
+function fixed(A,n=4,tn) {
+  return A.map(e=>e.map(t=>(s=t.toFixed(n),tn?+s:s)))
+}
+
+function ws (A, n=1) {
+  let H = HR(A)[1]
+  const ra = getMRC(H)[0] - 1
+  while(n--){
+    const s = H[ra][ra] + H[ra-1][ra-1]
+    const t = H[ra][ra]*H[ra-1][ra-1] - H[ra][ra-1]*H[ra-1][ra]
+    const M = MPO(MPO(MT(H,H),MPO(s,H,MMath.time),MMath.sub),createMatrix(ra+1,ra+1,(r,c)=>r===c?t:0),MMath.add)
+    const [Q,R] = Givens(M)
+    H = MT(MT(transpose(Q),H),Q)
+  }
+  return H
+}
+
+function fs (A, n=1) {
+  let H = HR(A)[1]
+  const ra = getMRC(H)[0] - 1
+  while(n--){
+    const s = H[ra][ra] + H[ra-1][ra-1]
+    const t = H[ra][ra]*H[ra-1][ra-1] - H[ra][ra-1]*H[ra-1][ra]
+    const x = H[0][0]**2 + H[0][1]*H[1][0] - s*H[0][0] + t
+    const y = H[1][0]*(H[0][0]+H[1][1]-s)
+    const z = H[1][0]*H[2][1]
+    const p = Hh([x,y,z].concat(Array(ra-2).fill(0)))[1]
+    const P = HR(MT(MT(transpose(p),H),p))[0]
+    const Q = MT(p,P)
+    H = MT(MT(transpose(Q),H),Q)
+  }
+  return H
+}
+
+function replaceM (A, B, [rs,re],[cs,ce]) {
+  for(let r=rs;r<re;r++){
+    for(let c=cs;c<ce;c++){
+      A[r][c] = B[r-rs][c-cs]
+    }
+  }
+}
+
+function sfs (A, tol=1) {
+  let Hb = HR(A)[1]
+  const H=(a,b)=>Hb[a-1][b-1]
+  const n = getMRC(Hb)[0]
+  let p = n
+  const e = 1e-15
+  const log = []
+  let time = 0
+  while(tol--&&p>2){
+    let q = p -1
+    let s = H(q,q) + H(p,p)
+    let t = H(q,q)*H(p,p) - H(p,q)*H(q,p)
+    let x = H(1,1)**2 + H(1,2)*H(2,1) - s*H(1,1) + t
+    let y = H(2,1)*(H(1,1)+H(2,2)-s)
+    let z = H(2,1)*H(3,2)
+    for(let k=0;k<p-2;k++){
+      let P = Hh([x,y,z])[1]
+      let tP = transpose(P)
+      let r = Math.max(1,k)
+      replaceM(Hb,MT(tP,splitM(Hb,[k,k+3],[r-1,n])),[k,k+3],[r-1,n])
+      r = Math.min(k+4,p)
+      replaceM(Hb,MT(splitM(Hb,[0,r],[k,k+3]),P),[0,r],[k,k+3])
+      x = H(k+2,k+1)
+      y = H(k+3,k+1)
+      if (k < p-3) {
+        z = H(k+4,k+1)
+      }
+    }
+    let P = Hh([x,y])[1]
+    let tP = transpose(P)
+    replaceM(Hb,MT(tP,splitM(Hb,[q-1,p],[p-3,n])),[q-1,p],[p-3,n])
+    replaceM(Hb,MT(splitM(Hb,[0,p],[p-2,p]),P),[0,p],[p-2,p])
+    time++
+    log.push([time, p, H(p,p-1),H(p-1,p-2)])
+    if(Math.abs(H(p,q))<e*(Math.abs(H(q,q))+Math.abs(H(p,p)))){
+      Hb[p-1][q-1] = 0
+      p = p - 1
+      q = p - 1
+    } else if (Math.abs(H(p-1,q-1))<e*(Math.abs(H(q-1,q-1))+Math.abs(H(q,q)))) {
+      Hb[p-2][q-2] = 0
+      p = p - 2
+      q = p - 1
+    }
+  }
+  console.log(log)
+  return Hb
 }
